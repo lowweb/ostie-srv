847972e71d9a12fc845625452b36197126a1181c
lowweb <hugo-boy@mail.ru>
2023-07-19 21:57:06 +1000

    migrate to firefox

--- get_imdb_nm_films.py
+++ get_imdb_nm_films.py
@@ -1,16 +1,14 @@
-#import requests
-# from lxml import html
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 
-def read_file(filename):
-    with open(filename) as input_file:
-        text = input_file.read()
-    input_file.close()
-    return text
+# def read_file(filename):
+#     with open(filename) as input_file:
+#         text = input_file.read()
+#     input_file.close()
+#     return text
 
 def get_media_info (ARTIST_ID,SEARCH_SONG,USER_AGENT,ACCEPT_LANGUAGE):
     """
@@ -19,76 +17,78 @@ def get_media_info (ARTIST_ID,SEARCH_SONG,USER_AGENT,ACCEPT_LANGUAGE):
       """
 
     url = f'https://www.imdb.com/name/{ARTIST_ID}/'
+    m_results = []
     #headers = {'User-Agent': USER_AGENT,'Accept-Language': ACCEPT_LANGUAGE}
     #response = requests.get(url, headers = headers)
     options = Options()
-    options.add_argument("-headless")
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
     options.add_argument(f'user-agent={USER_AGENT}')
-    # options.add_experimental_option('prefs', {'intl.accept_languages': ACCEPT_LANGUAGE})
-    driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver",options=options)
+    options.set_preference('intl.accept_languages', ACCEPT_LANGUAGE)
+    driver = webdriver.Firefox(options=options)
     driver.get(url)
-    btnElement = driver.find_element(By.ID, "name-filmography-filter-soundtrack")
-    driver.execute_script("arguments[0].click();", btnElement);
-    # добавить клик на кнопку "больше"
-    time.sleep(1)  # seconds
+
+    try:
+        btnElement = driver.find_element(By.ID, "name-filmography-filter-soundtrack")
+        # if button @soundtrack do not press
+        if 'filmography-selected-chip-filter' not in btnElement.get_attribute('class').split():
+            driver.execute_script("arguments[0].click();", btnElement);
+    except:
+        return m_results
+
+    # some case if we see @see_also
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(3)
     soup = BeautifulSoup(driver.page_source,'html.parser')
 
     # print(ARTIST_ID)
     # print(SEARCH_SONG)
-    m_results = []
 
-    # тестовый парсинг по файлу потом убрать
+
+    # txt parcing
     # text = read_file('data/sound.html')
     # soup = BeautifulSoup(text,'html.parser')
 
     soundtrack_block = soup.find('div', {'class': 'filmo-section-soundtrack'})
     if soundtrack_block:
-        # print('yes')
-        films_list = soup.find('div', {'class': 'filmo-section-soundtrack'}).find_next('ul', {
-            'class': 'ipc-metadata-list'}).find_all('li', {'class': 'ipc-metadata-list-summary-item'})
-        # m_posters =[]
-        # проходимся по фильмам/сериалам/шоу
+        soundtrack_blocks=soup.find('div', {'class': 'filmo-section-soundtrack'}).find_next('div',{'class': 'sc-526668ed-3'})
+        soundtrack_accordion = soundtrack_blocks.find_all('div',{'class': 'ipc-accordion'})
+        for item in soundtrack_accordion:
+            item_title = item.find('span', {'class': 'ipc-accordion__item__title'}).find('li',{'class': 'ipc-inline-list__item'}).text.split()
+            if ('Previous' in item_title):
+                soundtrack_previous_lists = item
+        films_list =  soundtrack_previous_lists.find('ul', {'class': 'ipc-metadata-list'}).find_all('li', {'class': 'ipc-metadata-list-summary-item'})
+        # parsing all film in lists
         for item in films_list:
-            # постер
             if item.find('img', {'class': 'ipc-image'}):
                 m_poster = item.find('img', {'class': 'ipc-image'}).get('src')
             else:
                 m_poster = 'null'
-            # ссылка на фильм
             m_href = item.find('a').get('href')
-            # id фильма пока пусто
             m_id = m_href.split('/')[2]
-            # название фильма
-            m_name = item.find('a').get('aria-label')
-            # print(m_name)
-            # год может быть пустым
+            # m_name = item.find('a').get('aria-label')
+            m_name = item.find('div', {'class': 'ipc-metadata-list-summary-item__c'}).find_next('a', {'class': 'ipc-metadata-list-summary-item__t'}).text.strip()
             m_year = item.find('div', {'class': 'ipc-metadata-list-summary-item__cc'}).find_next('span', {
                 'class': 'ipc-metadata-list-summary-item__li'}).text.strip()
             m_year = m_year if m_year != '' else 'unknown'
-            # print(m_year)
-            # песни
             m_songs = []
             m_episodes = []
-            songs_upper_case = []
+            songs_in_uppercase = []
             songs_blocks = item.find('div', {'class': 'ipc-metadata-list-summary-item__tc'}).find_all('span', {
                 'class': 'ipc-metadata-list-summary-item__li'})
-            # создаем массив всех песен в фильме
             for block in songs_blocks:
                 block = block.text
                 if ('performer:' in block):
-                    # print(song)
                     songs = block.strip().split(',')
-                    # print ('split =',manySongs)
                     for song in songs:
-                        # находим левые и правые кавычки, все что без "" откидываем
                         lf_pos = song.find('"') + 1
                         rgh_pos = song[lf_pos:].find('"') + lf_pos
                         if lf_pos and rgh_pos:
                             song = song[lf_pos:rgh_pos].strip()
                             m_songs.append(song)
-                            songs_upper_case.append(song.upper())
-                            # print(m_songs)
-            if (len(SEARCH_SONG) != 0 and SEARCH_SONG.upper() in songs_upper_case):
+                            songs_in_uppercase.append(song.upper())
+            if (len(SEARCH_SONG) != 0 and SEARCH_SONG.upper() in songs_in_uppercase):
                 m_results.append({'mId': m_id,
                                   'mLink': m_href,
                                   'mName': m_name,
@@ -103,10 +103,5 @@ def get_media_info (ARTIST_ID,SEARCH_SONG,USER_AGENT,ACCEPT_LANGUAGE):
                                   'mPoster': m_poster,
                                   'mEpisodes': m_episodes,
                                   'mSongs': m_songs})
-            # print(m_name)
-            # print(m_songs)
-    # print(len(m_results))
-    # print(m_results)
-        # print('==================================================')
     driver.close()
     return m_results
--- get_selenium.py
+++ get_selenium.py
@@ -2,35 +2,26 @@ from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import time
 
 
-#
-# def read_file(filename):
-#     with open(filename) as input_file:
-#         text = input_file.read()
-#     input_file.close()
-#     return text
 
-option = webdriver.ChromeOptions()
-option.add_argument("--headless")
-# option.add_argument("--host-resolver-rules=MAP www.google-analytics.com 127.0.0.1")
-option.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36')
-# webdriver.Chrome(options=option)
-# options = Options()
-# options.add_argument('--headless')
-# options.add_argument('--no-sandbox')
-# option.add_argument('--lang=en-EN,ru;q=0.8,en-US;q=0.5,en;q=0.3')
-option.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
-# options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:109.0) Gecko/20100101 Firefox/115.0")
-#
-driver = webdriver.Chrome(options=option)
+def read_file(filename):
+    with open(filename) as input_file:
+        text = input_file.read()
+    input_file.close()
+    return text
 
-# driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+# options.add_argument(f'user-agent={USER_AGENT}')
+# options.add_experimental_option('prefs', {'intl.accept_languages': ACCEPT_LANGUAGE})
+driver = webdriver.Firefox(options=options)
 
-
-url = 'https://www.imdb.com/name/nm1736962/'
+url = 'https://www.imdb.com/name/nm3058944'
 # url = 'https://www.python.org/'
 driver.get(url)
 
@@ -39,10 +30,10 @@ btnElement = driver.find_element(By.ID, "name-filmography-filter-soundtrack")
 driver.execute_script("arguments[0].click();", btnElement);
 # print(inputElement)
 # добавить клик на кнопку "больше"
-time.sleep(2)  # seconds
+time.sleep(3)  # seconds
 
 # # тестовый парсинг по файлу потом убрать
-# text = read_file('data/civil.html')
+# text = read_file('data/Smiths.html')
 # soup = BeautifulSoup(text,'html.parser')
 
 #отдаем всю страницу Bsoap
@@ -69,7 +60,7 @@ if  soundtrack_block:
         m_id = m_href.split('/')[2]
         # название фильма
         m_name = item.find('a').get('aria-label')
-        # print(m_name)
+        print(m_name)
         # год может быть пустым
         m_year = item.find('div', {'class': 'ipc-metadata-list-summary-item__cc'}).find_next('span',{'class': 'ipc-metadata-list-summary-item__li'}).text.strip()
         m_year = m_year if m_year != '' else 'unknown'
@@ -116,4 +107,4 @@ print(len(m_results))
 # print(m_results)
 
 
-driver.close()
+# driver.close()
